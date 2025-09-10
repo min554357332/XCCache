cda15480dc2c21e8928e3a0661db0c7350e3510a
ld <ld@lddeMacBook-Pro.local>
2025-09-10 15:38:17 +0800

    update

--- Sources/Public/Ex/XCCache_Ex.swift
+++ Sources/Public/Ex/XCCache_Ex.swift
@@ -23,7 +23,7 @@ public extension NECache {
     static private func _readLocal(_ filename: String, dataPreprocessor: XCCacheDataPreprocessor) async throws {
         if await Manager.shared.exists(forKey: String.init(describing: type(of: self))) == false,
            let localFilePath = Bundle.main.url(forResource: filename, withExtension: nil) {
-            let data = try Data(contentsOf: localFilePath)
+            let data = try Data(contentsOf: localFilePath).base64EncodedData()
             let preprocess = try await dataPreprocessor.preprocess(data: data)
             try await JSONDecoder().decode(self, from: preprocess).w(dataPreprocessor: dataPreprocessor)
         }
